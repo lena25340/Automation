aa7179c0a193c64a7d3f978417558b4580851add
lena25340 <grishinaalyon4@yandex.ru>
2024-09-01 16:05:00 +0300

    Lesson_3

---
+++ Lesson_3/Mailing.py
@@ -0,0 +1,13 @@
+class  Mailing:
+    def __init__(self, to_address, from_address, cost, track):
+        self.to_address = to_address
+        self.from_address = from_address
+        self.cost = cost
+        self.track = track
+
+        def ___str__(self):
+            return (f"Отправление { self.track}  из  { self.from_address}"
+                    f"в {self.to_address}. Стоимость {self.cost} рублей.") 
+        
+
+        
---
+++ Lesson_3/smartphone.py
@@ -0,0 +1,18 @@
+class Smartphone:
+
+    def __init__(self, phone_brand, phone_model, subscriber_number):
+        self.phone_brand = phone_brand
+        self.phone_model = phone_model
+        self.subscriber_number= subscriber_number
+
+    def brand(self):
+        print(self.phone_brand)
+
+    def model(self):
+        print(self.phone_model)
+
+    def number(self):
+        print(self.subscriber_number)
+
+    def brandmodelnumber(self):
+        print(self.phone_brand, self.phone_model, self.subscriber_number)
