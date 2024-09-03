95da4a351f0f9386a8b815f9ba56c0f3dbe427f5
lena25340 <grishinaalyon4@yandex.ru>
2024-09-03 23:01:40 +0300

    3 задание

--- Lesson_3/lesson_3_tack_2.py
+++ Lesson_3/lesson_3_tack_2.py
@@ -1,4 +1,4 @@
-from Smartphone import Smartphone
+from Smartphone1 import Smartphone
 
 catalog = [Smartphone("Apple", "14 pro", "+79501357194"),
            Smartphone("Xiaomi", "Xiaomi12", "+79546284751"),
