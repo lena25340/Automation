b848c329d3f8461046025a75abb83452979da9b7
lena25340 <grishinaalyon4@yandex.ru>
2024-10-16 22:03:06 +0300

    7 homework

--- Lesson_7/Swag_Labs/AuthPage.py
+++ Lesson_7/Swag_Labs/AuthPage.py
@@ -1,8 +1,11 @@
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
+class AuthPage:
+    def __init__(self, driver):
+        self.browser = driver
+        
+        self.browser.get("https://www.saucedemo.com")
 
-def regisration_fieds(self):
+    def regisration_fieds(self):
         self._name = (By.ID, "user-name")
         self._pass = (By.ID, "password")
         self._log_button = (By.ID, "login-button")
@@ -10,5 +13,3 @@ def regisration_fieds(self):
         self.browser.find_element(*self._name).send_keys("standard_user")
         self.browser.find_element(*self._pass).send_keys("secret_sauce")
         self.browser.find_element(*self._log_button).click()
-        
-        
---
+++ Lesson_7/Swag_Labs/Shopcontainer.py
@@ -0,0 +1,24 @@
+from selenium.webdriver.common.by import By
+
+class ShopContainer:
+    def __init__(self,browser):
+        self.browser = browser
+
+    def checkout (self):
+        self.check = (By.ID,"checkout")
+        self.browser.find_element(*self.check).click()
+    def info (self):
+        self.first_name = (By.ID,"first-name")
+        self.browser.find_element(*self.first_name).send_keys("Лена")
+        self.last_name = (By.ID,"last-name")
+        self.browser.find_element(*self.last_name).send_keys("Гришина ")
+        self.postalcode = (By.ID,"postal-code")
+        self.browser.find_element(*self.postalcode).send_keys("241020")
+        self.continue_button = (By.ID,"continue")      
+        self.browser.find_element(*self.continue_button).click() 
+    def price(self):
+        total_price = self.browser.find_element(By.CSS_SELECTOR,".summary_total_label")
+        total = total_price.text.strip().replace("Total: $", "")
+        return total
+        
+      
